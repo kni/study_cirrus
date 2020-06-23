9b93cf68e0519af1d7a223124345f002104859f3
Nick Kostyria <kostirya@gmail.com>
2020-06-23 13:37:03 +0300

    :-)

--- run.sh
+++ run.sh
@@ -2,15 +2,9 @@
 set -v
 pwd
 echo $HOME
-echo $PATH
 
-declare -a rargs
+/usr/local/bin/mlton
 
-echo '#!bash' > foo
-echo 'set -v' >> foo
-cat /usr/local/bin/mlton >> foo
-bash foo
-
-# git clone https://github.com/MLton/mlton.git mlton_git
-# cd mlton_git
-# gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
+git clone https://github.com/MLton/mlton.git mlton_git
+cd mlton_git
+gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
