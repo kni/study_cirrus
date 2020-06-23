bf72810d0832a1e2c8b43d2c0b9bb510cd17aa7a
Nick Kostyria <kostirya@gmail.com>
2020-06-23 14:02:55 +0300

    :-)

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ echo $HOME
 
 /usr/local/bin/mlton
 
-git clone https://github.com/MLton/mlton.git mlton_git
+git-lite clone https://github.com/MLton/mlton.git mlton_git
 cd mlton_git
 gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
