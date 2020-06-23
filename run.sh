23cf2404a0db4dc81ed6892443ed18991ad19eac
Nick Kostyria <kostirya@gmail.com>
2020-06-23 09:57:08 +0300

    :-)

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!sh
 pwd
+
+echo $PATH
+
+whereis mlton
+
 mlton
+
+git clone https://github.com/MLton/mlton.git mlton_git
+cd mlton_git
+gmake CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
