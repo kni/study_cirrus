cce5bf818dea42afb1d68c2440992abf14e14657
Nick Kostyria <kostirya@gmail.com>
2020-06-23 11:59:25 +0300

    :-)

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 pwd
 echo $HOME
 echo $PATH
-export PATH="$PATH:/usr/local/bin"
-echo $PATH
 
+bash -c 'echo hello bash'
 
-mlton
+ls /usr/local/bin/
+echo XXX
+ls /usr/local/bin/mlton
 
-whereis mlton
-/usr/local/bin/mlton
+/usr/local/bin/mlton -h
 
-git clone https://github.com/MLton/mlton.git mlton_git
-cd mlton_git
-gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
+# git clone https://github.com/MLton/mlton.git mlton_git
+# cd mlton_git
+# gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
