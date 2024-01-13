b6bdb247a7522803e1a64bced4bfb5b447a8fa63
kni <kostirya@gmail.com>
2024-01-13 08:57:03 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-set -v
-echo OK
-pwd
-echo $HOME
-ls
+set -e
+ln -s $HOME/seamonkey-2.53.13 /usr/ports/www/seamonkey
+cd /usr/ports/www/seamonkey
+make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
+make package
+
+mkdir -p $HOME/pkg
+mv work/pkg/* $HOME/pkg
