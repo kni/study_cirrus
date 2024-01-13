76f572ff305d2df98d3972fb80f7c84f33dfacc3
kni <kostirya@gmail.com>
2024-01-13 10:28:09 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,11 @@
 set -v
 set -e
 
+fetch http://ftp.freebsd.org/pub/FreeBSD/ports/ports/ports.tar.gz
+rm -rf /usr/ports/*
+tar -xzf ports.tar.gz -C /usr
+
+
 grep PERL5_DEFAULT /usr/ports/Mk/bsd.default-versions.mk
 echo ======
 ls -l -d /usr/ports/lang/perl5*
@@ -9,8 +14,8 @@ echo ======
 
 ln -s /tmp/cirrus-ci-build/seamonkey-2.53.13 /usr/ports/www/seamonkey
 cd /usr/ports/www/seamonkey
-make PERL5_DEFAULT=5.34 build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
-make PERL5_DEFAULT=5.34 package
+make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
+make package
 
 mkdir -p /tmp/cirrus-ci-build/pkg
 mv work/pkg/* /tmp/cirrus-ci-build/pkg
