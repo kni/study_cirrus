9c8e6d1893b33bd970eea75534e7127d6d359471
kni <kostirya@gmail.com>
2024-01-13 09:23:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -v
 set -e
+
+pkg search perl
+echo ======
+
+pkg upgrade -y
+echo ======
+
+pkg search perl
+echo ======
+
 ln -s /tmp/cirrus-ci-build/seamonkey-2.53.13 /usr/ports/www/seamonkey
 cd /usr/ports/www/seamonkey
 make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
