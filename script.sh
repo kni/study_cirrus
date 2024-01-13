22231e804a7efe72b09018dd2b04423f6804bbac
kni <kostirya@gmail.com>
2024-01-13 09:56:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,15 @@ echo ======
 ls -l -d /usr/ports/lang/perl5*
 echo ======
 
+portsnap fetch update 
+echo ======
+
+grep PERL5_DEFAULT /usr/ports/Mk/bsd.default-versions.mk
+echo ======
+ls -l -d /usr/ports/lang/perl5*
+echo ======
+
+
 ln -s /tmp/cirrus-ci-build/seamonkey-2.53.13 /usr/ports/www/seamonkey
 cd /usr/ports/www/seamonkey
 make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
