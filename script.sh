e8329e909f3597abe371d5034afa0a9901b4c922
kni <kostirya@gmail.com>
2024-01-13 10:19:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,19 +7,10 @@ echo ======
 ls -l -d /usr/ports/lang/perl5*
 echo ======
 
-portsnap cron update 
-echo ======
-
-grep PERL5_DEFAULT /usr/ports/Mk/bsd.default-versions.mk
-echo ======
-ls -l -d /usr/ports/lang/perl5*
-echo ======
-
-
 ln -s /tmp/cirrus-ci-build/seamonkey-2.53.13 /usr/ports/www/seamonkey
 cd /usr/ports/www/seamonkey
-make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
-make package
+make PERL5_DEFAULT=5.34 build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
+make PERL5_DEFAULT=5.34 package
 
 mkdir -p /tmp/cirrus-ci-build/pkg
 mv work/pkg/* /tmp/cirrus-ci-build/pkg
