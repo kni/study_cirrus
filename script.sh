2c8ee6eb8c94aad887d7188abc985fb1f0e68dcf
kni <kostirya@gmail.com>
2024-01-13 09:10:50 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-ln -s $HOME/seamonkey-2.53.13 /usr/ports/www/seamonkey
+ln -s /tmp/cirrus-ci-build/seamonkey-2.53.13 /usr/ports/www/seamonkey
 cd /usr/ports/www/seamonkey
 make build-depends-list run-depends-list | cut -c 12- | xargs pkg install -A -y
 make package
 
-mkdir -p $HOME/pkg
-mv work/pkg/* $HOME/pkg
+mkdir -p /tmp/cirrus-ci-build/pkg
+mv work/pkg/* /tmp/cirrus-ci-build/pkg
