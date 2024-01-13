62144fad651ea3a1cbe8c6f3ff3eeac6114ec693
kni <kostirya@gmail.com>
2024-01-13 08:09:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
-#!bash
+#!/bin/sh
 set -v
+echo OK
 pwd
 echo $HOME
-
-bash foo.sh
-
-# bash /usr/local/bin/mlton
-#
-# /usr/local/bin/mlton
-#
-# git clone https://github.com/MLton/mlton.git mlton_git
-# cd mlton_git
-# gmake MLTON=/usr/local/bin/mlton CC=/usr/bin/clang WITH_GMP_INC_DIR=/usr/local/include WITH_GMP_LIB_DIR=/usr/local/lib
+ls
