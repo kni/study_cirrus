c7b52a8f5c1547422c28777c3a9f81d249329377
kni <kostirya@gmail.com>
2024-01-13 08:58:50 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
+
 echo OK
 
 echo -n 'pwd: ' && pwd
+echo -n '$HOME: ' && $HOME
 echo -n 'uname: ' && uname -a
 echo -n 'freebsd-version: ' && freebsd-version
 
@@ -13,6 +15,6 @@ dmesg | head -n 120
 echo
 top -b
 
-
 echo
-ls -l /usr/ports/README
+echo ls
+ls
